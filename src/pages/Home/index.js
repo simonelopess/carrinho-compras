11b1a6536200ab4a463f805ea26a81f7561348e4
Simone Lopes <sisilopes20@gmail.com>
2023-04-01 19:04:21 +0100

    Cria a funcionalidade inicial do contexto de carrinho

---
+++ src/pages/Home/index.js
@@ -0,0 +1,105 @@
+import React, { useState, useContext } from "react";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import Product from "../../components/Product";
+import { CartContext } from "../../context/CartContext";
+
+export default function Home() {
+  const navigation = useNavigation();
+  const { cart } = useContext(CartContext);
+
+  const [products, setProducts] = useState([
+    {
+      id: "1",
+      name: "Coca cola",
+      price: 19.9,
+    },
+    {
+      id: "2",
+      name: "Chocolate",
+      price: 6.5,
+    },
+    {
+      id: "4",
+      name: "Queijo 500g",
+      price: 15,
+    },
+    {
+      id: "5",
+      name: "Batata frita",
+      price: 23.9,
+    },
+    {
+      id: "6",
+      name: "Guarana lata",
+      price: 6.0,
+    },
+  ]);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.cartContent}>
+        <Text style={styles.title}>Lista de Produtos</Text>
+        <TouchableOpacity
+          style={styles.cartButton}
+          onPress={() => navigation.navigate("Cart")}
+        >
+          <View style={styles.dot}>
+            <Text style={styles.dotText}>{cart?.length}</Text>
+          </View>
+          <Feather name="shopping-cart" size={30} color="#000" />
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        style={styles.list}
+        data={products}
+        renderItem={({ item }) => <Product data={item} />}
+        keyExtractor={(item) => String(item.id)}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fafafa",
+    paddingEnd: 14,
+    paddingStart: 14,
+  },
+  cartContent: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 24,
+    marginBottom: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  dot: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "red",
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    position: "absolute",
+    zIndex: 99,
+    bottom: -2,
+    left: -4,
+  },
+  dotText: {
+    fontSize: 12,
+  },
+});
